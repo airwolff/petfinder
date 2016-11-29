6760adc0ea7f803e0c2ca870acdbc752c188af04
Andy Wolff <andy.wolff@gmail.com>
2016-11-29 14:33:33 -0600

    stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,19 @@
-
-var app = angular.module('app', []);
-
-app.controller('Controller', ['$http', function($http) {
-  console.log('firstController up and running');
-  self.message = "Welcome!"
-}]);
+var app = angular.module('app', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/dogs', {
+            templateUrl: '/views/templates/dog.html',
+            controller: 'DogController',
+            controllerAs: 'dog',
+        })
+        .when('/cats', {
+            templateUrl: '/views/templates/cat.html',
+            controller: 'CatController',
+            controllerAs: 'cat',
+        })
+        .when('/barn', {
+            templateUrl: '/views/templates/barn.html',
+            controller: 'BarnController',
+            controllerAs: 'barn',
+        })
+}, ]);
